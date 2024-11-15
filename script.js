7d53528320a93058ef61736242aa2b229190855a
Emiakpor <emiakporibiri@yahoo.com>
2024-11-15 08:59:20 +0100

    Added js functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+let count =0;
+
+const displayCount = document.getElementById('displayCount');
+const incrementButton = document.getElementById('incrementButton');
+const decrementButton = document.getElementById('decrementButton');
+const resetButton = document.getElementById('resetButton');
+
+incrementButton.addEventListener('click', () => {
+    displayCount.innerText = ++count;
+});
+
+decrementButton.addEventListener('click', () => {
+    displayCount.innerText = --count;
+});
+
+resetButton.addEventListener('click', () => {
+    count=0;
+    displayCount.innerText = count;
+});
